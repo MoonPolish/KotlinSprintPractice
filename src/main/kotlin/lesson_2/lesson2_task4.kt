bb1966745f97dd6959d5fad92877df9e7bac381d
MoonPolish <polinagorshkova200@gmail.com>
2024-11-17 12:15:04 +0300

    KS-2-4 rework task

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,16 @@
 package org.example.lesson_2
 
+const val ONE_HUNDRED_PERCENT = 100
+
 fun main() {
 
-    val buffPercentage = 1.2
+    val buffPercentage = 20
 
     val crystalOre = 7
     val metalOre = 11
 
-    val bonusCrystalOre = (crystalOre * buffPercentage - crystalOre).toInt()
-    val bonusMetalOre = (metalOre * buffPercentage - metalOre).toInt()
+    val bonusCrystalOre = crystalOre * buffPercentage / ONE_HUNDRED_PERCENT
+    val bonusMetalOre = metalOre * buffPercentage / ONE_HUNDRED_PERCENT
 
     println("Бонус кристаллической руды составит: +$bonusCrystalOre")
     println("Бонус металлической руды составит: +$bonusMetalOre")
