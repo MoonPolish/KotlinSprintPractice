311523b149088178a8c0c4c3435c1237f6373470
MoonPolish <polinagorshkova200@gmail.com>
2024-11-17 15:04:01 +0300

    KS-3-2 names and ages

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var surname = "Андреева"
+    var name = "Татьяна"
+    var patronymic = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $patronymic $age лет")
+
+    surname = "Сидорова"
+    name = "Татьяна"
+    patronymic = "Сергеевна"
+    age = 22
+
+    println("$surname $name $patronymic $age года")
+
+}
