5f49ad53577bb41536e98f11c2e6bd6a7a3dc81e
MoonPolish <polinagorshkova200@gmail.com>
2024-11-17 14:53:34 +0300

    KS-3-1 greetings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var greetings = "Добрый день, "
+    var name = "Человек"
+
+    println(greetings + name)
+
+    greetings = "Добрый вечер, "
+    name = "Люк Скайоукер"
+
+    println(greetings + name)
+
+}
