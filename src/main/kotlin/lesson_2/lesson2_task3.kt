75ef7f708d5e7de5e919b7fe9506612f2f93b73e
MoonPolish <polinagorshkova200@gmail.com>
2024-11-17 12:07:45 +0300

    KS-2-3 rework task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,19 +1,20 @@
 package org.example.lesson_2
 
+const val BABYLONIAN_NUMBER = 60
+const val TIME_FORMAT = 24
+
 fun main() {
 
-    val babylonianNumber = 60
-    val timeFormat = 24
 
-    val timeDepartureString = "9:39"
-    val (departureHours, departureMinutes) = timeDepartureString.split(":").map { it.toInt() }
-    val departureTimeInMinutes = departureHours * babylonianNumber + departureMinutes
+    val departureHours = 9
+    val departureMinutes = 39
+    val departureTimeInMinutes = departureHours * BABYLONIAN_NUMBER + departureMinutes
 
     val travelTimeInMinutes = 457
 
     val arrivalTimeInMinutes = departureTimeInMinutes + travelTimeInMinutes
-    val arrivalMinutes = arrivalTimeInMinutes % babylonianNumber
-    val arrivalHours = arrivalTimeInMinutes / babylonianNumber % timeFormat
+    val arrivalMinutes = arrivalTimeInMinutes % BABYLONIAN_NUMBER
+    val arrivalHours = arrivalTimeInMinutes / BABYLONIAN_NUMBER % TIME_FORMAT
 
     val finalArrivalTime = String.format("%02d:%02d", arrivalHours, arrivalMinutes)
 
