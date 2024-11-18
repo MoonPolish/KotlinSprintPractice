e2a48d148a7072b0052da91e7f3d0d06af8b7047
MoonPolish <polinagorshkova200@gmail.com>
2024-11-18 19:56:34 +0300

    KS-3-3 rework

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -3,8 +3,13 @@ package org.example.lesson_3
 fun main() {
 
     val multiplicand = 8
+    val factor = 9
+    val result = StringBuilder()
 
-    for(i in 1..10)
-        println("$multiplicand x $i = ${multiplicand * i}")
+    for (i in 1..factor) {
+        result.append("$multiplicand x $i = ${multiplicand * i}\n")
+    }
+
+    println(result)
 
 }
