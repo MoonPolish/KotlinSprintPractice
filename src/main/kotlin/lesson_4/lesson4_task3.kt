22317870bb43a56a7cb7e4fe11d77fbd23858734
MoonPolish <polinagorshkova200@gmail.com>
2024-11-19 06:29:09 +0300

    KS-4-3 rework

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,9 +1,9 @@
 package org.example.lesson_4
 
-const val SUNNY_WEATHER = true
-const val SUNSHADE = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val HUMIDITY = 20
-const val CURRENT_TIME_OF_YEAR = "не зима"
+const val CURRENT_TIME_OF_YEAR = "зима"
 
 fun main() {
 
@@ -12,6 +12,10 @@ fun main() {
     val humidity = 20
     val currentTimeOfYear = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherToday == SUNNY_WEATHER) && (sunshade == SUNSHADE) && (humidity == HUMIDITY) && (currentTimeOfYear == CURRENT_TIME_OF_YEAR)}")
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+            "${(weatherToday == IS_SUNNY) && 
+            (sunshade == IS_AWNING_OPEN) && 
+            (humidity == HUMIDITY) && 
+            (currentTimeOfYear != CURRENT_TIME_OF_YEAR)}")
 
 }
