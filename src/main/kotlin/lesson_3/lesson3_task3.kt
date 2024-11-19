bf99ddab1b930d105a2d4fdc82bdf42895086862
MoonPolish <polinagorshkova200@gmail.com>
2024-11-19 19:33:31 +0300

    KS-3-3 final rework

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -2,13 +2,18 @@ package org.example.lesson_3
 
 fun main() {
 
-    val multiplicand = 8
-    val result = StringBuilder()
+    val multiplicand = 9
 
-    for (i in 1..9) {
-        result.append("$multiplicand x $i = ${multiplicand * i}\n")
-    }
-
-    println(result)
+    println("""
+        $multiplicand x 1 = ${multiplicand * 1}
+        $multiplicand x 2 = ${multiplicand * 2}
+        $multiplicand x 3 = ${multiplicand * 3}
+        $multiplicand x 4 = ${multiplicand * 4}
+        $multiplicand x 5 = ${multiplicand * 5}
+        $multiplicand x 6 = ${multiplicand * 6}
+        $multiplicand x 7 = ${multiplicand * 7}
+        $multiplicand x 8 = ${multiplicand * 8}
+        $multiplicand x 9 = ${multiplicand * 9}
+    """.trimIndent())
 
 }
