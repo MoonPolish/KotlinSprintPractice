a34017e6c64ae07764f57865cbd2311d2f20267d
MoonPolish <polinagorshkova200@gmail.com>
2024-11-25 19:23:50 +0300

    KS-4-4 final rework

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,26 +1,23 @@
 package org.example.lesson_4
 
-const val ARMS_AND_ABS = false
-const val LEGS_AND_BACK = true
-
 fun main() {
 
     val numberDay = 5
     val isEven = numberDay % 2 == 0
 
-    val hands = "Упражнения для рук:"
-    val legs = "Упражнения для ног:"
-    val back = "Упражнения для спины:"
-    val abs = "Упражнения для пресса:"
+    val hands = "Упражнения для рук: "
+    val legs = "Упражнения для ног: "
+    val back = "Упражнения для спины: "
+    val abs = "Упражнения для пресса: "
 
     val maxLength = maxOf(hands.length, legs.length, back.length, abs.length)
 
     println(
         """
-        ${hands.padEnd(maxLength)} ${ARMS_AND_ABS != !isEven}
-        ${legs.padEnd(maxLength)} ${LEGS_AND_BACK == isEven}
-        ${back.padEnd(maxLength)} ${LEGS_AND_BACK == isEven}
-        ${abs.padEnd(maxLength)} ${ARMS_AND_ABS != !isEven}
+        ${hands.padEnd(maxLength)}${!isEven}
+        ${legs.padEnd(maxLength)}${isEven}
+        ${back.padEnd(maxLength)}${isEven}
+        ${abs.padEnd(maxLength)}${!isEven}
     """.trimIndent()
     )
 
