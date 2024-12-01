7616b2122b011e8eeb8b2cadf010d892c5638e17
MoonPolish <polinagorshkova200@gmail.com>
2024-12-01 19:56:56 +0300

    KS-6-4 rework

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,11 +1,10 @@
 package org.example.lesson_6
 
-import kotlin.random.Random
-
 fun main() {
 
     var attempts = 5
-    val hiddenNumber = Random.nextInt(1, 10)
+    val hiddenNumber = (1..9)
+    val randomNumber = hiddenNumber.random()
 
     println("Угадайте число от 1 до 9. Осталось попыток $attempts")
 
@@ -14,10 +13,12 @@ fun main() {
         val inputNumber = readln().toInt()
 
         if (inputNumber == 0 || inputNumber < 1 || inputNumber > 9) {
+            val numbers = 1..10; if (inputNumber in numbers) {
+            }
             println("Пожалуйста, введите целое число от 1 до 9.")
             continue
         }
-        if (inputNumber == hiddenNumber) {
+        if (inputNumber == randomNumber) {
             println("Это была великолепная игра!")
             return
         } else {
@@ -25,7 +26,7 @@ fun main() {
             println("Неверно. Осталось $attempts попыток.")
         }
     }
-    println("Было загадано число $hiddenNumber.")
+    println("Было загадано число $randomNumber.")
 }
 
 
