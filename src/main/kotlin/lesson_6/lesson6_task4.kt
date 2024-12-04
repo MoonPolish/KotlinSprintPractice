b7b31d73adffd916c6e3d51712e75a223bb8ddb4
MoonPolish <polinagorshkova200@gmail.com>
2024-12-04 19:06:56 +0300

    KS-6-4 final rework

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -12,8 +12,7 @@ fun main() {
         println("Введите число: ")
         val inputNumber = readln().toInt()
 
-        val numbers = 1..9
-        if (inputNumber !in numbers) {
+        if (inputNumber !in hiddenNumber) {
             println("Пожалуйста, введите целое число от 1 до 9.")
             continue
         }
