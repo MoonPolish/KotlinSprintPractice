ac5d20223535d5c00fc90073e28442a4a795098a
MoonPolish <polinagorshkova200@gmail.com>
2024-12-09 19:52:39 +0300

    KS-8-2 new ingridients

---
+++ src/main/kotlin/lesson_8/lesson8_taks3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val ingridients = arrayOf("яйцо", "помидор", "зелень", "соль", "перец")
+
+    print("Введите ингридиент, который Вы хотите найти: ")
+    val inputIngridient = readln()
+
+    if (ingridients.contains(inputIngridient)) println("Ингредиент [$inputIngridient] в рецепте есть.")
+    else println("Такого ингредиента в рецепте нет.")
+
+}
