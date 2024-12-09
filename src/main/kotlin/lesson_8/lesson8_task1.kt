e507ff89d462dc7ade3cd2026ea2644cf56f8dbd
MoonPolish <polinagorshkova200@gmail.com>
2024-12-09 06:15:09 +0300

    KS-8-1 views per week

---
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val arrayViews = intArrayOf(2364, 123878, 32918, 341, 3278, 290, 2345)
+    val sumViews = arrayViews.sum()
+
+    println("Количество просмотров за неделю: $sumViews")
+
+}
+
+
+
