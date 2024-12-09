37260a5d1638e18d8977170b818bc835b0e01b90
MoonPolish <polinagorshkova200@gmail.com>
2024-12-09 05:40:26 +0300

    KS-6-5 rework

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -6,29 +6,12 @@ fun main() {
 
     var attempts = 3
 
-    println("Авторизация нового пользователя. Введите новый логин и пароль для регистрации: ")
-
-    val newLogin = readln()
-    val newPassword = readln()
-
-    println("Введите свой логин и пароль, чтобы войти в систему: ")
-
-    do {
-        val login = readln()
-        val password = readln()
-
-        if (login != newLogin || password != newPassword) {
-            println("Неверный логин или пароль. Пожалуйста, введите данные заново.")
-            continue
-        }
-        println("Авторизация прошла успешно.")
-
-        while (attempts > 0) {
-            val num1 = Random.nextInt(1, 10)
-            val num2 = Random.nextInt(1, 10)
-            val sum = num1 + num2
-            print("Докажите, что Вы не робот и решите простой пример: $num1 + $num2 = ")
-            val inputNumber = readln().toInt()
+            while (attempts > 0) {
+                val num1 = Random.nextInt(1, 10)
+                val num2 = Random.nextInt(1, 10)
+                print("Докажите, что Вы не робот и решите простой пример: $num1 + $num2 = ")
+                val sum = num1 + num2
+                val inputNumber = readln().toInt()
 
             if (inputNumber == sum) {
                 println("Добро пожаловать!")
@@ -39,5 +22,4 @@ fun main() {
             }
         }
         println("Доступ запрещён.")
-    } while (attempts > 0)
 }
