c534dd6c6b5e8d55ab366522e2c46fae8113bdf5
MoonPolish <polinagorshkova200@gmail.com>
2024-12-23 06:09:02 +0300

    KS-9-4 rework

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -1,17 +1,12 @@
 package org.example.lesson_9
 
-const val QUANTITY_OF_INGRIDIENTS = 5
-
 fun main() {
 
     println("Введите 5 ингридиентов через запятую и пробел:")
     val inputIngridients = readln()
-    val ingridients = inputIngridients.split(", ")
-    val sortedIngridients = ingridients.sorted()
+    val ingridients = inputIngridients.split(", ").sorted()
 
-    for (ingridient in sortedIngridients) {
-        if (sortedIngridients.size == QUANTITY_OF_INGRIDIENTS) {
-            println(ingridient)
-        }
+    for (ingridient in ingridients) {
+        println(ingridient)
     }
 }
