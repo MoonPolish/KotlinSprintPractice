029968639396c90eb0abf5d37c818fc7b2cefbe8
MoonPolish <polinagorshkova200@gmail.com>
2025-01-15 06:29:40 +0300

    KS-11-1 class instance

---
+++ src/main/kotlin/lesson_11/Main.kt
@@ -0,0 +1,35 @@
+package org.example.lesson_11
+
+fun main() {
+
+    val user1 = User(
+
+        id = 1,
+        login = "DobriyBrobovik",
+        password = "randoMPassWord127",
+        email = "DobriyBrobovik@gmail.com",
+
+    )
+
+    val user2 = User(
+
+        id = 2,
+        login = "kvadrober",
+        password = "kvadrober230_032",
+        email = "randommail@gmail.com"
+
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.email)
+
+    println()
+
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.email)
+
+}
