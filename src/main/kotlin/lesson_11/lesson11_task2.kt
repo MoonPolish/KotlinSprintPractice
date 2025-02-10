9717152ec658c7cba3ae14c9bcaa69feed6de471
MoonPolish <polinagorshkova200@gmail.com>
2025-02-10 19:37:12 +0300

    KS-11-2 user2

---
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -0,0 +1,46 @@
+package org.example.lesson_11
+
+fun main() {
+    val user = User2(
+        username = "username",
+        password = "1234567890"
+    )
+    user.setBioFromConsole()
+    user.changePassword()
+
+    user.printInfo()
+}
+
+class User2(
+    val username: String,
+    var password: String,
+    var bio: String? = null,
+) {
+
+    fun printInfo() {
+        println("Имя пользователя: $username")
+        println("bio: $bio")
+    }
+
+    fun setBioFromConsole() {
+        println("Введите своё bio: $bio")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Введите текущий пароль:")
+        val currentPassword = readln()
+
+        if (currentPassword == password) {
+            println("Текущий пароль корректный. Введите новый пароль:")
+            val newPassword = readln()
+
+            if (newPassword != null && newPassword.isNotEmpty()) {
+                password = newPassword
+                println("Пароль изменён.")
+            } else {
+                println("Новый пароль не может быть пустым.")
+            }
+        }
+    }
+}
