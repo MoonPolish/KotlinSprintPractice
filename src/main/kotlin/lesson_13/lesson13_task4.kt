c7582834ddd775c5193cf1dff3b70c37da576114
MoonPolish <polinagorshkova200@gmail.com>
2025-02-26 09:59:26 +0300

    KS-13-4 refactor

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -4,14 +4,34 @@ class PhoneDirectory3(
     val name: String,
     val phoneNumber: Long?,
     val company: String?,
-)
+) {
+    companion object {
+        fun showContacts(phoneBook: List<PhoneDirectory3>) {
+            if (phoneBook.isEmpty()) {
+                println("Телефонная книга пуста.")
+                return
+            }
+
+            println("Все контакты:")
+            phoneBook.forEachIndexed { index, phoneDirectory3 ->
+                println("${index + 1}. Имя: ${phoneDirectory3.name}")
+                println("   Телефон: ${phoneDirectory3.phoneNumber}")
+                println("   Компания: ${phoneDirectory3?.company}")
+            }
+        }
+    }
+
+}
 
 fun addContact(phoneBook: MutableList<PhoneDirectory3>) {
     println("Введите имя: ")
-    val name = readln()!!
+    val name = readln()?.takeIf { it.isNotEmpty() } ?: run {
+        println("Имя не может быть пустым.")
+        return
+    }
 
     println("Введите номер телефона: ")
-    val phoneNumberStr = readln()!!
+    val phoneNumberStr = readln()
     val phoneNumber = phoneNumberStr.toLongOrNull()
 
     if (phoneNumber == null) {
@@ -27,20 +47,6 @@ fun addContact(phoneBook: MutableList<PhoneDirectory3>) {
     println("Контакт успешно добавлен!")
 }
 
-fun showContacts(phoneBook: List<PhoneDirectory3>) {
-    if (phoneBook.isEmpty()) {
-        println("Телефонная книга пуста.")
-        return
-    }
-
-    println("Все контакты:")
-    phoneBook.forEachIndexed { index, phoneDirectory3 ->
-        println("${index + 1}. Имя: ${phoneDirectory3.name}")
-        println("   Телефон: ${phoneDirectory3.phoneNumber}")
-        println("   Компания: ${phoneDirectory3?.company}")
-    }
-}
-
 fun main() {
     val phoneBook = mutableListOf<PhoneDirectory3>()
 
@@ -51,9 +57,9 @@ fun main() {
         println("3. Выход")
 
         println("Введите команду: ")
-        when (readln()!!.toInt()) {
+        when (readln()?.toInt()) {
             1 -> addContact(phoneBook)
-            2 -> showContacts(phoneBook)
+            2 -> PhoneDirectory3.showContacts(phoneBook)
             3 -> break
             else -> println("Некорректная команда. Попробуйте снова.")
         }
