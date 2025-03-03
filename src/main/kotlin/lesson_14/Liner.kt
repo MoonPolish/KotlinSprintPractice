59ef1f8a083977c9446b2c83b02535870d3257c6
MoonPolish <polinagorshkova200@gmail.com>
2025-03-03 10:21:42 +0300

    KS-14-1 refactor

--- src/main/kotlin/lesson_14/Liner.kt
+++ src/main/kotlin/lesson_14/Liner.kt
@@ -11,3 +11,40 @@ open class Liner(
         println("Вместимость пассажиров: $passengerCapacity")
     }
 }
+
+class IcebreakerShip(
+    name: String = "Ледокол",
+    speed: Int = 10,
+    passengerCapacity: Int = 20,
+    val isPricksTheIce: Boolean = true,
+) : Liner(name, speed, passengerCapacity) {
+    override fun displayInfo() {
+        super.displayInfo()
+        println("Этот корабль может колоть лёд: $isPricksTheIce")
+    }
+}
+
+class CargoShip(
+    name: String = "Грузовой корабль",
+    speed: Int = 20,
+    passengerCapacity: Int = 50,
+    val cargoCapacity: Int = 1000,
+) : Liner(name, speed, passengerCapacity) {
+    override fun displayInfo() {
+        super.displayInfo()
+        println("Грузоподъёмность: $cargoCapacity тонн.")
+    }
+}
+
+fun main() {
+    val liner = Liner()
+    val cargo = CargoShip()
+    val icebreaker = IcebreakerShip()
+
+    println("Информация о кораблях:")
+    liner.displayInfo()
+    println()
+    cargo.displayInfo()
+    println()
+    icebreaker.displayInfo()
+}
