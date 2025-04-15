c201b7b35a3da3fb99159c022a2277c8696745a7
MoonPolish <polinagorshkova200@gmail.com>
2025-04-15 18:02:58 +0300

    KS-19-1 fish

---
+++ src/main/kotlin/lesson19_task1.kt
@@ -0,0 +1,21 @@
+package org.example
+
+enum class Fish(val fishName: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок");
+
+    companion object {
+        fun printFishes() {
+            println("Вы можете добавить следующих рыб в свой аквариум:")
+            values().forEach { fish ->
+                println("- ${fish.fishName}")
+            }
+        }
+    }
+}
+
+fun main() {
+    println(Fish.printFishes())
+}
